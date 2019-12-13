917b2929aa0c4bad7e2bad2471cec2a9f29916e3
Aleksey Zimin <azimin@gttb.com>
2019-12-13 19:13:21 +0200

    Refactor code

--- src/components/TodoSection.js
+++ src/components/TodoSection.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import { Grid } from '@material-ui/core';
 import List from './List';
 import Comments from './Comments';
-const TodoSection = () =>{
-    
-    return(
-        <div className="table">
-            <Grid container justify="center" className="todosec"> 
-                <Grid item  xs={12} md={6} style={{minHeight:600}}>
-                    <List />
-                </Grid>
-                <Grid item  xs={12} md={6}>
-                    <Comments />
-                </Grid> 
-            </Grid>
-        </div>
-    )
-}
+
+const TodoSection = () => {
+	return (
+		<div className="table">
+			<Grid container justify="center" className="todosec">
+				<Grid item xs={12} md={6} style={{ minHeight: 600 }}>
+					<List/>
+				</Grid>
+				<Grid item xs={12} md={6}>
+					<Comments/>
+				</Grid>
+			</Grid>
+		</div>
+	);
+};
 
 export default TodoSection;
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,47 +1,53 @@
-const initialState={
-    locale: "en",
-    itemList: [
 
-    ],
-    comments:[],
-    active: null,
-    newToDoId:0,
-    commentId:0,
-}
 
-const reducer = (state=initialState, action) =>{
-    switch(action.type){
-        case "ADD_TODO":
-            return{
-                ...state,
-                itemList:[...state.itemList, action.payload]
-            }
-        case "GET_INDEX":
-            return{
-                ...state,
-                newToDoId: state.newToDoId + 1
-            }
-        case "ACTIVATE_TODO":
-            return{
-                ...state,
-                active: action.payload
-            }
-        case "DELETE_TODO":
-            let result = state.comments.filter(item=>item.id !== action.payload2)
-            return{
-                ...state,
-                itemList: action.payload,
-                comments: result,
-                active: null
-            }
-        case "ADD_COMMENT":
-            return{
-                ...state,
-                comments: [...state.comments, action.payload],
-                commentId: state.commentId + 1
-            }
-        default: return state
-    }
-    return state;
-}
+const initialState = {
+	locale: 'en',       //  TODO REMOVE
+	itemList: [],
+	comments: [],
+	active: null,
+	newToDoId: 0,
+	commentId: 0,
+};
+
+const reducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case 'ADD_TODO': {
+			return {
+				...state,
+				itemList: [ ...state.itemList, payload ],
+			};
+		}
+		case 'GET_INDEX': {
+			return {
+				...state,
+				newToDoId: state.newToDoId + 1,
+			};
+		}
+		case 'ACTIVATE_TODO': {
+			return {
+				...state,
+				active: payload.id,
+			};
+		}
+		case 'DELETE_TODO': {
+			let result = state.comments.filter(item => item.id !== payload.id);
+			return {
+				...state,
+				itemList: payload.item,
+				comments: result,
+				active: null,
+			};
+		}
+		case 'ADD_COMMENT': {
+			return {
+				...state,
+				comments: [ ...state.comments, payload ],
+				commentId: state.commentId + 1,
+			};
+		}
+		default: {
+			return state;
+		}
+	}
+};
 export default reducer;
