69102d52d7811e6a4bfea96701fd734793aee830
Aleksey Zimin <azimin@gttb.com>
2019-12-13 14:29:47 +0200

    Updates

---
+++ precache-manifest.23399d1b4a349669f5ca79529e634de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845a0584e052ef72b96fd24ee543b24e",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "a500615c07a31df62bdf",
+    "url": "/todos/static/css/main.3b7c29f0.chunk.css"
+  },
+  {
+    "revision": "6baa22f65346fc6b334d",
+    "url": "/todos/static/js/2.2228dc13.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/todos/static/js/2.2228dc13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a500615c07a31df62bdf",
+    "url": "/todos/static/js/main.0fd408b6.chunk.js"
+  },
+  {
+    "revision": "c8bba319e22f25e70062",
+    "url": "/todos/static/js/runtime-main.48996770.js"
+  }
+]);
